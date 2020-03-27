adc674dd244203d98fa0b4e6006f1923fcf0e307
Stephanie smith <smithse4@gmail.com>
2020-03-26 20:03:05 -0400

    updated file structure

--- index.js
+++ index.js
@@ -1,134 +1,77 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-var api = require(‘./utils/api’);
+const api = require("./utils/api.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub user name?",
+    name: "userName"
+  },
+  {
+    type: "input",
+    message: "What is your project title",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Describe your project.",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What installation information is needed to run your program?",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "What usage information is needed to run your program?",
+    name: "usage"
+  },
+  {
+    type: "list",
+    message: "What license would you like to use?",
+    name: "license",
+    choices: ["MIT", "APACHE", "GPL", "BSD", "None"]
+  },
+  {
+    type: "input",
+    message: "What or who has contributed to your project?",
+    name: "contributing"
+  },
+  {
+    type: "input",
+    message: "What tests have you run on this program?",
+    name: "tests"
+  }
+];
 
 function writeToFile(fileName, data) {}
 
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(answer => {
+    console.log(answer);
+    const queryUrl = `https://api.github.com/users/${answer.userName}`;
+    axios
+      .get(queryUrl)
+      .then(function(response) {
+        console.log('?????????????', response.data.avatar_url)
+        const userInput = generateMarkdown(answer, response.data.avatar_url);
+        fs.writeFile("created-README.md", userInput, err => {
+          if (err) {
+            return console.log(err);
+          }
+    
+          console.log("Your README has been created");
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        return err
+      });
+  });
+}
 
 init();
-
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your GitHub user name?",
-      name: "userName"
-    },
-    {
-      type: "input",
-      message: "What is your project title",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "Describe your project.",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "What installation information is needed to run your program?",
-      name: "installation"
-    },
-    {
-      type: "input",
-      message: "What usage information is needed to run your program?",
-      name: "usage"
-    },
-    {
-      type: "list",
-      message: "What license would you like to use?",
-      name: "license",
-      choices: ["MIT", "APACHE-2.0", "GPL-3.0", "BSD-3", "None"]
-    },
-    {
-      type: "input",
-      message: "What or who has contributed to your project?",
-      name: "contributing"
-    },
-    {
-      type: "input",
-      message: "What tests have you run on this program?",
-      name: "tests"
-    }
-  ])
-  .then(
-    ({
-      userName,
-      title,
-      description,
-      installation,
-      usage,
-      license,
-      contributing,
-      tests
-    }) => {
-      const queryUrl = `https://api.github.com/users/${userName}`;
-
-      axios
-        .get(queryUrl)
-        .then(function(response) {
-          var profilePic = response.data.avatar_url;
-          // // console.log(response.data.avatar_url);
-          console.log(response);
-
-          fs.writeFile("created-README.md", buildReadMe, err => {
-            if (err) {
-              return console.log(err);
-            }
-
-            console.log("Your README has been created");
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-
-      const buildReadMe = `# ${title};
-
-![GitHub license](https://img.shields.io/badge/License-${license}-blueviolet.svg)](https://github.com/${userName}/${title})
-
-${description}
-
-## Table of Contents
-
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)
-    
-## Installation
-
-${installation}
-
-## Usage
-
-${usage}
-
-## License
-
-${license}
-
-## Contributing
-
-${contributing}
-
-## Tests
-
-${tests}
-
-## Questions
-
-<img src="" alt="avatar" style="border-radius: 16px" width="30" />
-
-  `;
-    }
-  )
-  .catch(err => {
-    console.log(err);
-  });
